87df95882c68cd7a3b21e4e7d524c3246213e212
Donnavan Stadd <donnystaddon@gmail.com>
2023-07-02 09:06:37 +0000

    The Filter Method

--- filter.js
+++ filter.js
@@ -7,18 +7,18 @@
 
 // Simple Filtering
 const people = [
-  {
-      name: 'Michael',
-      age: 23,
-  },
-  {
-      name: 'Lianna',
-      age: 16,
-  },
-  {
-      name: 'Paul',
-      age: 18,
-  },
+    {
+        name: 'Michael',
+        age: 23,
+    },
+    {
+        name: 'Lianna',
+        age: 16,
+    },
+    {
+        name: 'Paul',
+        age: 18,
+    },
 ];
 
 const oldEnough = people.filter(person => person.age >= 21);
@@ -29,34 +29,39 @@ console.log(paul);
 
 // Complex Filtering
 const students = [
-  {
-      id: 1,
-      name: 'Mark',
-      profession: 'Developer',
-      skills: [
-          { name: 'javascript', yrsExperience: 1 },
-          { name: 'html', yrsExperience: 5 },
-          { name: 'css', yrsExperience: 3 },
-      ]
-  },
-  {
-      id: 2,
-      name: 'Ariel',
-      profession: 'Developer',
-      skills: [
-          { name: 'javascript', yrsExperience: 0 },
-          { name: 'html', yrsExperience: 4 },
-          { name: 'css', yrsExperience: 2 },
-      ]
-  },
-  {
-      id: 3,
-      name: 'Jason',
-      profession: 'Designer',
-      skills: [
-          { name: 'javascript', yrsExperience: 1 },
-          { name: 'html', yrsExperience: 1 },
-          { name: 'css', yrsExperience: 5 },
-      ]
-  },
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 5 },
+            { name: 'css', yrsExperience: 3 },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Ariel',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 0 },
+            { name: 'html', yrsExperience: 4 },
+            { name: 'css', yrsExperience: 2 },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        profession: 'Designer',
+        skills: [
+            { name: 'javascript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 1 },
+            { name: 'css', yrsExperience: 5 },
+        ]
+    },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = students => students.skills.filter(has5yearsExp).lenght > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
